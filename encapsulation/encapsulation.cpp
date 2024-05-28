67377e847626c93286c74f1a2c46f16411af88f8
dannyfadli <danyfdly95@gmail.com>
2024-05-28 10:57:30 +0700

    menambahkan int main untuk memanggil sistem sebelumnya

--- encapsulation/encapsulation.cpp
+++ encapsulation/encapsulation.cpp
@@ -20,5 +20,16 @@ public:
 
 int main()
 {
-    
+    remoteLampu lampuRumah;
+    lampuRumah.setSaklarno(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarno(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarno(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarno(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
 }
