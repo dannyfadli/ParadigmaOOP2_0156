adfcab795719bad92cefa601c5e2ef389b226be9
dannyfadli <danyfdly95@gmail.com>
2024-05-28 10:32:43 +0700

    menambahkan class base class

---
+++ NOoprinding/NOoprinding.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahkan final sesudah nama class
+//untuk mencegah adanya overrinding
+
+class baseclass
+{
+public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo saya function dari base class";
+    }
+};
+
+int main()
+{
+    
+}
